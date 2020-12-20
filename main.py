c8b7937f98c409b2e80e3034ac39ee48db9bdeee
Jean Dubois <jd-dev@laposte.net>
2020-12-20 18:54:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
 #!/usr/bin/env python3
 # coding:utf-8
 import turtle
-import random
 
-turtle.colormode(255)
-turtle.up()
-turtle.goto(-100, -100)
-turtle.down()
-a = 0
-for loop in range(100):
-    turtle.forward(200 - a)
-    turtle.left(90)
-    R = random.randrange(0, 257, 10)
-    B = random.randrange(0, 257, 10)
-    G = random.randrange(0, 257, 10)
-    turtle.color((R, G, B))
-    a += 2
-turtle.up()
-turtle.goto(1000, 1000)
+turtle1 = turtle.Turtle()
+turtle2 = turtle.Turtle()
+turtle3 = turtle.Turtle()
+
+turtle1.width(5)
+turtle2.width(5)
+turtle3.width(5)
+
+turtle1.color('red')
+turtle2.color('green')
+turtle3.color('blue')
+
+turtle1.up()
+turtle2.up()
+turtle3.up()
+turtle1.goto(-100, -100)
+turtle2.goto(0, -100)
+turtle3.goto(-50, -13.4)
+turtle1.down()
+turtle2.down()
+turtle3.down()
+
+a = 100
+for loop in range(2):
+    for loop2 in range(3):
+        turtle1.forward(a)
+        turtle1.left(120)
+        turtle2.forward(a)
+        turtle2.left(120)
+        turtle3.forward(a)
+        turtle3.left(120)
+    a = 50
+
+turtle1.forward(50)
+turtle2.forward(50)
+turtle3.forward(50)
+
+for loop in range(5):
+    turtle1.forward(a)
+    turtle1.left(120)
+    turtle2.forward(a)
+    turtle2.left(120)
+    turtle3.forward(a)
+    turtle3.left(120)
+
+turtle1.right(60)
+turtle2.right(60)
+turtle3.right(60)
+turtle1.forward(50)
+turtle2.forward(50)
+turtle3.forward(50)
+
+turtle1.up()
+turtle2.up()
+turtle3.up()
+turtle1.goto(500, 0)
+turtle2.goto(500, 0)
+turtle3.goto(500, 0)
 
 turtle.exitonclick()
